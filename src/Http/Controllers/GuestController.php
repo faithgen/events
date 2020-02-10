6d3d3632dadf1c43bda93bf2bcd322f2a23e3f0d
innoflash <innocentmazando@gmail.com>
2020-02-10 15:40:39 +0200

    wired guests migrations

---
+++ src/Http/Controllers/GuestController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Innoflash\Events\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class GuestController extends Controller
+{
+    //
+}
---
+++ src/database/migrations/2020_02_10_133806_create_guests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGuestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->string('id')->index()->primary();
+            $table->string('event_id', 150)->index();
+            $table->timestamps();
+
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guests');
+    }
+}
