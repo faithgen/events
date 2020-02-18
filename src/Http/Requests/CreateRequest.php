3b77f1fc29ceff4073e58e2753e6bdd54bbdf0aa
innoflash <innocentmazando@gmail.com>
2020-02-18 20:53:56 +0200

    sorted finally

--- src/Http/Requests/CreateRequest.php
+++ src/Http/Requests/CreateRequest.php
@@ -16,7 +16,7 @@ class CreateRequest extends FormRequest
     public function authorize()
     {
         return true;
-        return $this->user()->can('event.create', Event::class);
+        return $this->user()->can('create', Event::class);
     }
 
     /**
