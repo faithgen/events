92badee479d9c74ef12a8126c1ec7ad27d606fc9
innoflash <innocentmazando@gmail.com>
2020-04-04 15:47:35 +0200

    did nothing

--- src/Listeners/Guest/Saved/ProcessImage.php
+++ src/Listeners/Guest/Saved/ProcessImage.php
@@ -2,10 +2,9 @@
 
 namespace Innoflash\Events\Listeners\Guest\Saved;
 
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Innoflash\Events\Events\Guest\Saved;
 use Intervention\Image\ImageManager;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class ProcessImage implements ShouldQueue
 {
@@ -25,13 +24,14 @@ class ProcessImage implements ShouldQueue
      * Handle the event.
      *
      * @param  Saved  $event
+     *
      * @return void
      */
     public function handle(Saved $event)
     {
         if ($event->getGuest()->image()->exists()) {
-            $ogImage = storage_path('app/public/events/original/') . $event->getGuest()->image->name;
-            $thumb100 = storage_path('app/public/events/50-50/') . $event->getGuest()->image->name;
+            $ogImage = storage_path('app/public/events/original/').$event->getGuest()->image->name;
+            $thumb100 = storage_path('app/public/events/50-50/').$event->getGuest()->image->name;
 
             $this->imageManager->make($ogImage)->fit(50, 50, function ($constraint) {
                 $constraint->upsize();
